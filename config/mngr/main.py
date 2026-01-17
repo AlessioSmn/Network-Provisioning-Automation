3f31472dc0ccf8bab6bb9df7c932b2ff3990923c
lorenzo-vezzani <lorenzovezzani8@gmail.com>
2026-01-17 17:31:59 +0100

    main.py update allowing med adjusting

--- config/mngr/main.py
+++ config/mngr/main.py
@@ -6,10 +6,17 @@ USERNAME = "root"
 PASSWORD = "admin"
 INTERVAL = 60
 
+MY_AS = 65020
+
 PE1_IP = "192.168.0.1"
 PE2_IP = "192.168.0.2"
 GW1_IP = "192.168.0.3"
 GW2_IP = "192.168.0.4"
+
+UP1_IP = "172.16.0.2"
+UP2_IP = "172.16.1.2"
+UP1_AS = 65541
+UP2_AS = 65542
 
 ALL_PEERS = [PE1_IP, PE2_IP, GW1_IP, GW2_IP]
 
@@ -21,7 +28,13 @@ with open("traffic-matrices.json", "r") as f:
 
 matrix_index = 0
 
-def configure_bgp_params(gw_ip, route_map_name, bucket):
+def configure_bgp_params(gw_ip, route_outbound_map_name, route_inbound_map_name, bucket, other_gw, other_inbound_map_name):
+    my_up = UP1_IP if gw_ip == GW1_IP else UP2_IP
+    other_up = UP2_IP if gw_ip == GW1_IP else UP1_IP
+    my_up_as = UP1_AS if gw_ip == GW1_IP else UP2_AS
+    other_up_as = UP2_AS if gw_ip == GW1_IP else UP1_AS
+
+
     cmds = ['configure terminal']
 
     seq = 10
@@ -33,15 +46,28 @@ def configure_bgp_params(gw_ip, route_map_name, bucket):
         prefix = entry
         pl_name = f'PL-{prefix.replace("/", "_")}'
         cmds.append(f'ip prefix-list {pl_name} permit {prefix}')
-        cmds.append(f'route-map {route_map_name} permit {seq}')
+
+        if(entry == CU1_NET or entry == CU2_NET):
+            cmds.append(f'route-map {route_inbound_map_name} permit {seq}')
+        else:
+            cmds.append(f'route-map {route_outbound_map_name} permit {seq}')
+        
         cmds.append(f' match ip address prefix-list {pl_name}')
-        cmds.append(f' set local-preference 200')
+
+        if(entry == CU1_NET or entry == CU2_NET):
+            cmds.append(f' set local-preference 200')
+        else:
+            cmds.append(f' set metric 50')
+
         seq += 10
 
     for peer_ip in ALL_PEERS:
         if peer_ip != gw_ip: 
-            cmds.append(f'router bgp 65020')
-            cmds.append(f' neighbor {peer_ip} route-map {route_map_name} out')
+            cmds.append(f'router bgp {MY_AS}')
+            cmds.append(f' neighbor {peer_ip} route-map {route_outbound_map_name} out')
+
+    cmds.append(f' neighbor {my_up_as} remote-as')
+    cmds.append(f' neighbor {my_up} route-map {route_inbound_map_name} out')
 
     cmds.append('end')
 
@@ -56,6 +82,51 @@ def configure_bgp_params(gw_ip, route_map_name, bucket):
     stdin, stdout, stderr = ssh.exec_command(vtysh_cmd)
 
     print(f"=== Output from {gw_ip} ===")
+    print(stdout.read().decode())
+    print(stderr.read().decode())
+
+    ssh.close()
+
+    cmds = ['configure terminal']
+
+    seq = 10
+
+    # necessario settare un valore di MED più altro (default 0) anche sull'altro gateway
+
+    for entry in bucket:
+        prefix = entry
+        pl_name = f'PL-{prefix.replace("/", "_")}'
+        cmds.append(f'ip prefix-list {pl_name} permit {prefix}')
+
+        if(not(entry == CU1_NET or entry == CU2_NET)):
+            continue
+            
+        cmds.append(f'route-map {other_inbound_map_name} permit {seq}')
+        
+        cmds.append(f' match ip address prefix-list {pl_name}')
+            
+        cmds.append(f' set metric 100')
+
+        seq += 10
+
+    cmds.append(f'router bgp {MY_AS}')
+
+    cmds.append(f' neighbor {other_up} remote-as {other_up_as}')
+    cmds.append(f' neighbor {other_up} route-map {other_inbound_map_name} out')
+
+    cmds.append('end')
+
+    vtysh_cmd = 'vtysh ' + ' '.join([f'-c "{c}"' for c in cmds])
+
+    print(f"Executing the following command in GW {other_gw}:")
+    print(vtysh_cmd)
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(other_gw, username=USERNAME, password=PASSWORD)
+    stdin, stdout, stderr = ssh.exec_command(vtysh_cmd)
+
+    print(f"=== Output from {other_gw} ===")
     print(stdout.read().decode())
     print(stderr.read().decode())
 
@@ -101,16 +172,21 @@ while True:
     # bucket1 → GW1 (UP1)
     configure_bgp_params(
         gw_ip=GW1_IP,
-        route_map_name='SET-LP-UP1',
-        bucket=bucket1
+        route_outbound_map_name='SET-LP-GW1',
+        route_inbound_map_name='SET-MED-GW1',
+        bucket=bucket1,
+        other_gw=GW2_IP,
+        other_inbound_map_name='SET-MED-GW2'
     )
 
     # bucket2 → GW2 (UP2)
     configure_bgp_params(
         gw_ip=GW2_IP,
-        route_map_name='SET-LP-UP2',
-        bucket=bucket2
+        route_outbound_map_name='SET-LP-GW2',
+        route_inbound_map_name='SET-MED-GW2',
+        bucket=bucket2,
+        other_gw=GW1_IP,
+        other_inbound_map_name='SET-MED-GW1'
     )
 
     matrix_index = (matrix_index + 1) % len(traffic_matrices)
-    time.sleep(INTERVAL)
