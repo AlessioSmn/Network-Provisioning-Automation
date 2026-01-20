c584c2e451ec529a503697a9220e0832c223c99c
lorenzo-vezzani <lorenzovezzani8@gmail.com>
2026-01-20 18:16:27 +0100

    add filtering method for from-internet routes

--- config/mngr/main.py
+++ config/mngr/main.py
@@ -4,6 +4,8 @@ import paramiko
 
 USERNAME = "root"
 PASSWORD = "admin"
+
+# matrices are read with a defer of
 INTERVAL = 60
 
 MY_AS = 65020
@@ -13,6 +15,7 @@ PE2_IP = "192.168.100.12"
 GW1_IP = "192.168.100.21"
 GW2_IP = "192.168.100.22"
 
+# {lo_ip -> core_ip} this map is important, since mngr-node doesnt know about lo_ip
 lo_ip_map = {
     PE1_IP : "192.168.0.1",
     PE2_IP : "192.168.0.2",
@@ -30,51 +33,114 @@ ALL_PEERS = [PE1_IP, PE2_IP, GW1_IP, GW2_IP]
 CU1_NET = "192.0.2.0/24"
 CU2_NET = "192.51.100.0/24"
 
-with open("traffic-matrices.json", "r") as f:
-    traffic_matrices = json.load(f)
+# launch clear ip bgp cmd for desiderated peer inside ssh client specified
+def clear_bgp(peer_ip, ssh):
+    cmd = f"vtysh -c 'clear ip bgp {peer_ip} soft in'"
+    print(cmd)
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    stdout.channel.recv_exit_status()
+    out = stdout.read().decode()
+    err = stderr.read().decode()
+    if out:
+        print(f"[{peer_ip}] OUT: {out}")
+    if err:
+        print(f"[{peer_ip}] ERR: {err}")
 
-matrix_index = 0
 
-def configure_bgp_params(gw_ip, route_outbound_map_name, route_inbound_map_name, bucket, other_gw, other_inbound_map_name):
+def configure_filtering(gw_ip, route_inbound_map_name, bucket):
+    
+    cmds = ['configure terminal']
+
+    seq = 10
+
+    for entry in bucket:
+        # to internet route, not interesting here
+        if not (entry == CU1_NET or entry == CU2_NET):
+            continue
+
+        # buiding filter using route map
+        
+        prefix = entry
+        pl_name = f'PL-{prefix.replace("/", "_")}'
+        
+        cmds.append(f'ip prefix-list {pl_name} permit {prefix}')
+            
+        cmds.append(f'route-map {route_inbound_map_name} deny {seq}')
+        
+        cmds.append(f' match ip address prefix-list {pl_name}')
+
+        seq += 10
+    
+    # catch-all route map
+    cmds.append(f'route-map {route_inbound_map_name} permit 999')
+
+    # appling the route map
+    cmds.append(f'router bgp {MY_AS}')
+    for peer_ip in ALL_PEERS:
+        if(peer_ip == gw_ip):
+            continue
+
+        cmds.append(f' neighbor {peer_ip} remote-as {MY_AS}')
+        cmds.append(f' neighbor {peer_ip} route-map {route_inbound_map_name} out')
+
+    cmds.append('end')
+
+    vtysh_cmd = "vtysh \\\n" + " \\\n".join([f'    -c "{c}"' for c in cmds])
+
+    print(f"Executing the following command in GW {gw_ip}:")
+    print(vtysh_cmd)
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(lo_ip_map[gw_ip], username=USERNAME, password=PASSWORD)
+    stdin, stdout, stderr = ssh.exec_command(vtysh_cmd)
+
+    # refresh bgp updates
+    for peer_ip in ALL_PEERS:
+        if peer_ip != gw_ip:
+            clear_bgp(peer_ip=peer_ip, ssh=ssh)
+
+    print(f"=== Output from {gw_ip} ===")
+    print(stdout.read().decode())
+    print(stderr.read().decode())
+
+    ssh.close()
+
+
+def configure_bgp_local_pref(gw_ip, route_outbound_map_name, bucket):
+    # finds which my up is
     my_up = UP1_IP if gw_ip == GW1_IP else UP2_IP
-    other_up = UP2_IP if gw_ip == GW1_IP else UP1_IP
     my_up_as = UP1_AS if gw_ip == GW1_IP else UP2_AS
-    other_up_as = UP2_AS if gw_ip == GW1_IP else UP1_AS
 
     cmds = ['configure terminal']
 
     seq = 10
 
-    if not bucket:
-        return
-
     for entry in bucket:
+        # from internet route, not interesting here
+        if(entry == CU1_NET or entry == CU2_NET):
+            continue
+        
         prefix = entry
         pl_name = f'PL-{prefix.replace("/", "_")}'
-        cmds.append(f'ip prefix-list {pl_name} permit {prefix}')
+        
+        # buiding preference using route map
 
-        if(entry == CU1_NET or entry == CU2_NET):
-            cmds.append(f'route-map {route_inbound_map_name} permit {seq}')
-        else:
-            cmds.append(f'route-map {route_outbound_map_name} permit {seq}')
+        cmds.append(f'ip prefix-list {pl_name} permit {prefix}')
+            
+        cmds.append(f'route-map {route_outbound_map_name} permit {seq}')
         
         cmds.append(f' match ip address prefix-list {pl_name}')
 
-        if(entry == CU1_NET or entry == CU2_NET):
-            cmds.append(f' set metric 50')
-        else:
-            cmds.append(f' set local-preference 200')
+        cmds.append(f' set local-preference 200')
 
         seq += 10
 
+    # catch all preference
     cmds.append(f'route-map {route_outbound_map_name} permit 999')
 
-    for peer_ip in ALL_PEERS:
-        if peer_ip != gw_ip: 
-            cmds.append(f'router bgp {MY_AS}')
-            cmds.append(f' neighbor {peer_ip} remote-as {MY_AS}')
-            cmds.append(f' neighbor {peer_ip} route-map {route_inbound_map_name} out')
-
+    # appling the route map
+    cmds.append(f'router bgp {MY_AS}')
     cmds.append(f' neighbor {my_up} remote-as {my_up_as}')
     cmds.append(f' neighbor {my_up} route-map {route_outbound_map_name} in')
 
@@ -90,18 +156,10 @@ def configure_bgp_params(gw_ip, route_outbound_map_name, route_inbound_map_name,
     ssh.connect(lo_ip_map[gw_ip], username=USERNAME, password=PASSWORD)
     stdin, stdout, stderr = ssh.exec_command(vtysh_cmd)
 
+    # refresh bgp updates
     for peer_ip in ALL_PEERS:
         if peer_ip != gw_ip:
-            cmd = f"vtysh -c 'clear ip bgp {peer_ip} soft in'"
-            print(cmd)
-            stdin, stdout, stderr = ssh.exec_command(cmd)
-            stdout.channel.recv_exit_status()
-            out = stdout.read().decode()
-            err = stderr.read().decode()
-            if out:
-                print(f"[{peer_ip}] OUT: {out}")
-            if err:
-                print(f"[{peer_ip}] ERR: {err}")
+            clear_bgp(peer_ip=peer_ip, ssh=ssh)
 
     print(f"=== Output from {gw_ip} ===")
     print(stdout.read().decode())
@@ -109,114 +167,92 @@ def configure_bgp_params(gw_ip, route_outbound_map_name, route_inbound_map_name,
 
     ssh.close()
 
-    cmds = ['configure terminal']
 
-    seq = 10
 
-    # necessario settare un valore di MED più altro (default 0) anche sull'altro gateway
+with open("traffic-matrices.json", "r") as f:
+    traffic_matrices = json.load(f)
 
-    for entry in bucket:
-        prefix = entry
-        pl_name = f'PL-{prefix.replace("/", "_")}'
-        cmds.append(f'ip prefix-list {pl_name} permit {prefix}')
+matrix_index = 0
 
-        if(not(entry == CU1_NET or entry == CU2_NET)):
-            continue
-            
-        cmds.append(f'route-map {other_inbound_map_name} permit {seq}')
-        
-        cmds.append(f' match ip address prefix-list {pl_name}')
-            
-        cmds.append(f' set metric 100')
-
-        seq += 10
-
-    cmds.append(f'route-map {other_inbound_map_name} permit 999')
-    
-    cmds.append(f'router bgp {MY_AS}')
-
-    cmds.append(f' neighbor {other_up} remote-as {other_up_as}')
-    cmds.append(f' neighbor {other_up} route-map {other_inbound_map_name} out')
-
-    for peer_ip in ALL_PEERS:
-        if peer_ip != other_gw: 
-            cmds.append(f'router bgp {MY_AS}')
-            cmds.append(f' neighbor {peer_ip} remote-as {MY_AS}')
-            cmds.append(f' neighbor {peer_ip} route-map {other_inbound_map_name} out')
-
-    cmds.append('end')
-
-    vtysh_cmd = "vtysh \\\n" + " \\\n".join([f'    -c "{c}"' for c in cmds])
-
-    print(f"Executing the following command in GW {other_gw}:")
-    print(vtysh_cmd)
-
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(lo_ip_map[other_gw], username=USERNAME, password=PASSWORD)
-    stdin, stdout, stderr = ssh.exec_command(vtysh_cmd)
-
-    print(f"=== Output from {other_gw} ===")
-    print(stdout.read().decode())
-    print(stderr.read().decode())
-
-    ssh.close()
-
+# sleep 30s to ensure network fully operation (a regime)
 time.sleep(30)
+
 while True:
 
     current_matrix = traffic_matrices[matrix_index]
     
-    bucket1 = []
-    bucket2 = []
+    """
+    (Remember each entry of the traffic matrices provides the expeteced-traffic from a net_i to a net_j)
+
+    The greedy alghoritm exploits 2 buckets to determine the best traffic distribution across upstream links. In particular, it works like this:
+        1. retrieve a route
+        2. between the 2 buckets, the one with the less amount of expected-traffic is selcted
+        3. the route is inserted in the bucket and the total amount of traffic of the bucket is added with this route one's
+        4. if there are still other routes -> goto point-1, otherwise end
+
+    At the end, bucket1 will be used to determine which routes must have a 200 local-pref in GW1 (to-internet routes) and which routes must be filtered in 
+        GW2 (from-internet routes). The opposite goes for bucket2.
+
+    Extra: suppose we store a route from CU1 to internet and then a route, the route from CU2 to internet must be served from the same GW of the previous one.
+        It's not possible to have GW1 serving (CU1 -> net) and GW2 (CU2 -> net) 
+        (unless using other techniques such as traffic engineering or segment routing)
+    """
+    
+    bucket_gw1 = []
+    bucket_gw2 = []
     total1 = 0
     total2 = 0
 
     assigned_destinations = set()
 
-    print(f"Retrieving traffic matrix:")
     for entry in current_matrix:
         dst = entry["to"]
         traffic = entry["expected-traffic"]
 
-        print(f"{entry['from']} -> {dst} : {traffic}")
-
         if dst in assigned_destinations:
-            if dst in bucket1:
+            if dst in bucket_gw1:
                 total1 += traffic
             else:
                 total2 += traffic
         else:
             if total1 <= total2:
                 total1 += traffic
-                bucket1.append(dst)
+                bucket_gw1.append(dst)
             else:
                 total2 += traffic
-                bucket2.append(dst)
+                bucket_gw2.append(dst)
             
             assigned_destinations.add(dst)
 
-    print(f"Bucket1: {bucket1}, total1: {total1}")
-    print(f"Bucket2: {bucket2}, total2: {total2}")
+    print(f"Bucket1: {bucket_gw1}, total1: {total1}")
+    print(f"Bucket2: {bucket_gw2}, total2: {total2}")
 
-    # bucket1 → GW1 (UP1)
-    configure_bgp_params(
+    # bucket1 is used to set local-pref in GW1
+    configure_bgp_local_pref(
         gw_ip=GW1_IP,
         route_outbound_map_name='SET-LP-GW1',
-        route_inbound_map_name='SET-MED-GW1',
-        bucket=bucket1,
-        other_gw=GW2_IP,
-        other_inbound_map_name='SET-MED-GW2'
+        bucket=bucket_gw1
     )
 
-    # bucket2 → GW2 (UP2)
-    configure_bgp_params(
+    # bucket2 is used to set local-pref in GW2
+    configure_bgp_local_pref(
         gw_ip=GW2_IP,
         route_outbound_map_name='SET-LP-GW2',
-        route_inbound_map_name='SET-MED-GW2',
-        bucket=bucket2,
-        other_gw=GW1_IP,
-        other_inbound_map_name='SET-MED-GW1'
+        bucket=bucket_gw2
+    )
+
+    # bucket2 is used to impose filters in GW1 
+    configure_filtering(
+        gw_ip=GW1_IP,
+        route_inbound_map_name='SET-FILTERING-GW1',
+        bucket=bucket_gw2
+    )
+
+    # bucket1 is used to impose filters in GW2
+    configure_filtering(
+        gw_ip=GW2_IP,
+        route_inbound_map_name='SET-FILTERING-GW2',
+        bucket=bucket_gw1
     )
 
     matrix_index = (matrix_index + 1) % len(traffic_matrices)
